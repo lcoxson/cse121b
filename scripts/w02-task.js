3e3f36092123472ef95ae4a32105531c97b5ef37
lcoxson <coxson.laura@gmail.com>
2024-01-20 22:17:45 -0700

    Completed w02 Task

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -4,24 +4,31 @@
 
 /* Step 2 - Variables */
 let fullName = "Laura M. Coxson";
-let currentYear = 2024
-let profilePicture = images/headshot.jpg
+let currentYear = 2024;
+let profilePicture = "images/headshot.jpg";
 
 /* Step 3 - Element Variables */
-const nameElement = document.getElementById("name")
-const foodElement = document.getElementById("food")
-const yearElement = document.querySelector("#year")
-const imageElement = document.getElementById("profile image")
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+const imageElement = document.querySelector("img");
 
 /* Step 4 - Adding Content */
-
-
-
-
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute("src", profilePicture);
+imageElement.setAttribute("alt", "Profile image of ${fullName}");
 
 /* Step 5 - Array */
-
+const favoriteFoods = ["Brownies", "Chicken Parmesan", "Feijoada", "Pecan Pie", "French Toast"];
+foodElement.innerHTML = `${favoriteFoods}`;
+const newFood = "Peanut Butter and Banana Sandwich";
+favoriteFoods.push(newFood);
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+favoriteFoods.shift();
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+favoriteFoods.pop();
+foodElement.innerHTML += `<br>${favoriteFoods}`;
 
 
 
